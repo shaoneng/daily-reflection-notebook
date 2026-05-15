45577255ce85ed1e51aeef0dea90ca1385d910dc
shaoneng <wushaoneng@qq.com>
2026-05-15 11:36:12 +0800

    feat: delete individual notebook entries

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-import { onRequestGet as getDay, onRequestPost as saveDay } from "../functions/api/day/index.js";
+import { onRequestDelete as deleteDayEntry, onRequestGet as getDay, onRequestPost as saveDay } from "../functions/api/day/index.js";
 import { onRequestGet as exportDay } from "../functions/api/export/index.js";
 import { onRequestPost as reviewDay } from "../functions/api/review/index.js";
 import { onRequestGet as searchNotes } from "../functions/api/search/index.js";
@@ -7,6 +7,7 @@ import { onRequestGet as listTags } from "../functions/api/tags/index.js";
 const routes = {
   "GET /api/day": getDay,
   "POST /api/day": saveDay,
+  "DELETE /api/day": deleteDayEntry,
   "GET /api/export": exportDay,
   "POST /api/review": reviewDay,
   "GET /api/search": searchNotes,
